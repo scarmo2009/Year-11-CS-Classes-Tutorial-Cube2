9e8a6834d58e69614fe5309f061c442a45b5935c
scarlettmorrow <scarlettrqm2028@student.cis.edu.hk>
2025-11-20 08:01:07 +0800

    Cube 2

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,76 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this(side);
+        this.color = color;
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public void setSide(int side) {
+        basicCube.setSide(side);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int calculateSurfaceArea() {
+        return basicCube.calculateSurfaceArea();
+    }
+
+    public int calculateVolume() {
+        return basicCube.calculateVolume();
+    }
+
+    public Cube2 add(Cube2 otherCube) {
+        int a = this.getSide();
+        int b = otherCube.getSide();
+        int sumOfSquares = a * a + b * b;
+        int c = (int) Math.sqrt(sumOfSquares);
+
+        if (c * c != sumOfSquares) {
+            throw new IllegalArgumentException("Side lengths do not form a Pythagorean triple");
+        }
+
+        return new Cube2(c, this.color);
+    }
+
+    public Cube2 minus(Cube2 otherCube) {
+        int c = this.getSide();
+        int a = otherCube.getSide();
+        int differenceOfSquares = c * c - a * a;
+
+        if (differenceOfSquares <= 0) {
+            throw new IllegalArgumentException("Resulting side length would be less than 1");
+        }
+
+        int b = (int) Math.sqrt(differenceOfSquares);
+
+
+        if (b * b != differenceOfSquares) {
+            throw new IllegalArgumentException("Side lengths do not form a Pythagorean triple");
+        }
+
+        return new Cube2(b, this.color);
+    }
+
+    public boolean equals(Cube2 otherCube) {
+        if (otherCube == null) {
+            return false;
+        }
+        return this.getSide() == otherCube.getSide() &&
+                this.color.equals(otherCube.color);
+    }
 
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + basicCube.getSide() + ", color=\"" + color + "\"}";
     }
 }
